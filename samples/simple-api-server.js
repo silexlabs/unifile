85c2da186bd83040418c14fc19bcef6b0a581efe
JbIPS <jeanbaptiste.richardet@gmail.com>
2016-06-28 17:02:33 +0200

    Makes services stateless

--- samples/simple-api-server.js
+++ samples/simple-api-server.js
@@ -12,9 +12,15 @@ const unifile = new Unifile();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const serveStatic = require('serve-static');
+const session = require('express-session');
 
 app.use( bodyParser.json() );
 app.use(cookieParser());
+app.use(session({
+  secret: 'unifile',
+  resave: false,
+  saveUninitialized: false
+}));
 //app.use(serveStatic(__dirname+'/public', {index: 'index.html'}));
 
 // Fake connector
@@ -25,93 +31,106 @@ unifile.use(connector);
 
 // Register connector methods
 app.post('/:connector/authorize', function(req, res) {
-  var result = unifile.getAuthorizeURL(req.params.connector);
+  var result = unifile.getAuthorizeURL(req.session.unifile, req.params.connector);
   res.end(result);
 });
 
 // Search for a old session token in the cookies
 app.get('/', function(req, res){
-  if(req.cookies.unifileToken){
-    unifile.setAccessToken('GitHub', req.cookies.unifileToken);
-  }
-  res.sendFile(__dirname + '/public/index.html');
+  // Init unifile session in Express
+  req.session.unifile = req.session.unifile || {};
+
+  let response;
+  if(req.cookies.unifileToken)
+    response = unifile.setAccessToken(req.session.unifile, 'github', req.cookies.unifileToken)
+  else
+    response = unifile.clearAccessToken(req.session.unifile, 'github');
+
+  response.then(() => res.sendFile(__dirname + '/public/index.html'));
 });
 
 // List files and folders
 app.get(/\/(.*)\/ls\/(.*)/, function(req, res) {
-  unifile.readdir(req.params[0], '/' + req.params[1])
+  unifile.readdir(req.session.unifile, req.params[0], '/' + req.params[1])
   .then(function(result){
     res.send(result);
   })
   .catch(function(err){
+    console.error(err);
     res.status(400).send(err);
   });
 });
 
 app.put(/\/(.*)\/mkdir\/(.*)/, function(req, res) {
-  unifile.mkdir(req.params[0], '/' + req.params[1])
+  unifile.mkdir(req.session.unifile, req.params[0], '/' + req.params[1])
   .then(function(result){
     res.send(result);
   })
   .catch(function(err){
+    console.error(err);
     res.status(400).send(err);
   });
 });
 
 app.put(/\/(.*)\/put\/(.*)/, function(req, res) {
-  unifile.writeFile(req.params[0], '/' + req.params[1], req.body.content)
+  unifile.writeFile(req.session.unifile, req.params[0], '/' + req.params[1], req.body.content)
   .then(function(result){
     res.send(result);
   })
   .catch(function(err){
+    console.error(err);
     res.status(400).send(err);
   });
 });
 
 app.get(/\/(.*)\/get\/(.*)/, function(req, res) {
-  unifile.readFile(req.params[0], '/' + req.params[1])
+  unifile.readFile(req.session.unifile, req.params[0], '/' + req.params[1])
   .then(function(result){
     res.send(result);
   })
   .catch(function(err){
+    console.error(err);
     res.status(400).send(err);
   });
 });
 
 app.patch(/\/(.*)\/mv\/(.*)/, function(req, res) {
-  unifile.rename(req.params[0], '/' + req.params[1], '/' + req.body.destination)
+  unifile.rename(req.session.unifile, req.params[0], '/' + req.params[1], '/' + req.body.destination)
   .then(function(result){
     res.send(result);
   })
   .catch(function(err){
+    console.error(err);
     res.status(400).send(err);
   });
 });
 
 app.delete(/\/(.*)\/rm\/(.*)/, function(req, res) {
-  unifile.unlink(req.params[0], '/' + req.params[1])
+  unifile.unlink(req.session.unifile, req.params[0], '/' + req.params[1])
   .then(function(result){
     res.send(result);
   })
   .catch(function(err){
+    console.error(err);
     res.status(400).send(err);
   });
 });
 
 app.post(/\/(.*)\/cp\/(.*)/, function(req, res) {
-  var read = unifile.createReadStream(req.params[0], '/' + req.params[1]);
-  var write = unifile.createWriteStream(req.params[0], '/' + req.body.destination);
+  var read = unifile.createReadStream(req.session.unifile, req.params[0], '/' + req.params[1]);
+  var write = unifile.createWriteStream(req.session.unifile, req.params[0], '/' + req.body.destination);
   read.pipe(write).pipe(res);
 });
 
 // register callback url
 app.get('/oauth-callback', function(req, res) {
-  unifile.login(connector.name, req.query)
+  unifile.login(req.session.unifile, connector.name, req.query)
   .then(function(result){
     res.cookie('unifileToken', result);
     res.end('<script>window.close();</script>');
   })
   .catch(function(err){
+    console.error(err);
     res.status(500).send(err);
   });
 });
--- test/index.js
+++ test/index.js
@@ -14,7 +14,7 @@ describe('Unifile class', function() {
       expect(unifile).not.to.be.null;
       expect(unifile.connectors).to.be.an.instanceof(Map);
       expect(unifile.connectors.size).to.equal(0);
-      expect(unifile.useConnector).to.exist;
+      expect(unifile.use).to.exist;
     });
 
     it('create a new instance with config', function() {
@@ -27,24 +27,24 @@ describe('Unifile class', function() {
     });
   });
 
-  describe('useConnector()', function() {
+  describe('use()', function() {
     var unifile;
     beforeEach('Instanciation', function() {
       unifile = new Unifile();
     });
 
     it('throws an error if connector is undefined', function() {
-      expect(unifile.useConnector).to.throw(/Connector cannot be undefined/);
+      expect(unifile.use).to.throw(/Connector cannot be undefined/);
     });
 
     it('throws an error if connector does not have a name', function() {
-      var fn = function() { unifile.useConnector({}); };
+      var fn = function() { unifile.use({}); };
       expect(fn).to.throw(/Connector must have a name/);
     });
 
     it('register a new connector', function() {
       var connector = {name: 'test'};
-      unifile.useConnector(connector);
+      unifile.use(connector);
       expect(unifile.connectors.size).to.equal(1);
       expect(unifile.connectors.get(connector.name)).to.deep.equal(connector);
     });
@@ -63,14 +63,14 @@ describe('Unifile class', function() {
 
     it('throws an error if connector does not implement it', function() {
       var connector = {name: 'test'};
-      unifile.useConnector(connector);
+      unifile.use(connector);
       var fn = function() { unifile.getAuthorizeURL(connector.name); };
       expect(fn).to.throw(/This connector does not implement/);
     });
 
     it('returns a promise of the getAuthorizeURL function of the connector', function() {
       var connector = {name: 'test', getAuthorizeURL: function() {return new Promise.resolve();}};
-      unifile.useConnector(connector);
+      unifile.use(connector);
       expect(unifile.getAuthorizeURL(connector.name)).to.be.an.instanceof(Promise);
     });
   });
@@ -88,14 +88,14 @@ describe('Unifile class', function() {
 
     it('throws an error if connector does not implement it', function() {
       var connector = {name: 'test'};
-      unifile.useConnector(connector);
+      unifile.use(connector);
       var fn = function() { unifile.login(connector.name); };
       expect(fn).to.throw(/This connector does not implement/);
     });
 
     it('returns a promise of the login function of the connector', function() {
       var connector = {name: 'test', login: function() {return new Promise.resolve();}};
-      unifile.useConnector(connector);
+      unifile.use(connector);
       expect(unifile.login(connector.name)).to.be.an.instanceof(Promise);
     });
   });
@@ -113,14 +113,14 @@ describe('Unifile class', function() {
 
     it('throws an error if connector does not implement it', function() {
       var connector = {name: 'test'};
-      unifile.useConnector(connector);
+      unifile.use(connector);
       var fn = function() { unifile.ls(connector.name); };
       expect(fn).to.throw(/This connector does not implement/);
     });
 
     it('returns a promise of the ls function of the connector', function() {
       var connector = {name: 'test', ls: function() {return new Promise.resolve();}};
-      unifile.useConnector(connector);
+      unifile.use(connector);
       expect(unifile.ls(connector.name)).to.be.an.instanceof(Promise);
     });
   });
@@ -138,14 +138,14 @@ describe('Unifile class', function() {
 
     it('throws an error if connector does not implement it', function() {
       var connector = {name: 'test'};
-      unifile.useConnector(connector);
+      unifile.use(connector);
       var fn = function() { unifile.mkdir(connector.name); };
       expect(fn).to.throw(/This connector does not implement/);
     });
 
     it('returns a promise of the mkdir function of the connector', function() {
       var connector = {name: 'test', mkdir: function() {return new Promise.resolve();}};
-      unifile.useConnector(connector);
+      unifile.use(connector);
       expect(unifile.mkdir(connector.name)).to.be.an.instanceof(Promise);
     });
   });
@@ -163,14 +163,14 @@ describe('Unifile class', function() {
 
     it('throws an error if connector does not implement it', function() {
       var connector = {name: 'test'};
-      unifile.useConnector(connector);
+      unifile.use(connector);
       var fn = function() { unifile.put(connector.name); };
       expect(fn).to.throw(/This connector does not implement/);
     });
 
     it('returns a promise of the put function of the connector', function() {
       var connector = {name: 'test', put: function() {return new Promise.resolve();}};
-      unifile.useConnector(connector);
+      unifile.use(connector);
       expect(unifile.put(connector.name)).to.be.an.instanceof(Promise);
     });
   });
